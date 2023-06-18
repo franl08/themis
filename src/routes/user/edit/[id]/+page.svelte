4b536a11d831ec03e02485b9486762b23b7513cd
franl08 <franeves08@gmail.com>
2023-06-18 11:37:07 +0100

    edit profile

--- src/routes/user/edit/[id]/+page.svelte
+++ src/routes/user/edit/[id]/+page.svelte
@@ -2,24 +2,24 @@
 	// @ts-nocheck
 
 	import Button from '$lib/Button.svelte';
-	import Modal from '$lib/Modal.svelte';
 	import '../../../../app.css';
 	/** @type {import('./$types').PageData} */
 	export let data;
-	/** @type String */
-	export let username = data.username;
-	let modal = false;
+	let error = false;
 	let new_password = '';
 	let new_password2 = '';
+	let password = '';
 
-	function showModal() {
-		modal = !modal;
+	function updateData() {
+		// dar upload dos dados para a BD e aguardar resposta
+		// se a resposta for positiva, redirect, senão, mostrar erro
+		window.location = `/user/${data.username}`;
 	}
 </script>
 
-<div class="flex flex-col mt-5 ml-20">
-	<h1 class="flex text-6xl text-pink-700 uppercase font-bold">Editar Perfil</h1>
-	<div class="flex-col ml-10 mt-4 justify-center items-center">
+<div class="flex flex-col mt-5">
+	<h1 class="flex text-6xl ml-20 text-pink-700 uppercase font-bold">Editar Perfil</h1>
+	<div class="flex-col mt-4 justify-center items-center">
 		<form class="flex flex-col items-center after:justify-center mt-10">
 			<div class="flex flex-row">
 				<div class="flex flex-col w-40">
@@ -91,19 +91,43 @@
 					bind:value={new_password2}
 				/>
 			</div>
+			<div class="flex flex-row mt-4">
+				<div class="flex flex-col w-40">
+					<label for="password" class="flex text-xl dark:text-white">Password Atual: </label>
+				</div>
+				<input
+					type="password"
+					class="ml-5 rounded-md text-center w-60 overflow-hidden stroke-pink-700"
+					name="password"
+					placeholder="password"
+					bind:value={password}
+				/>
+			</div>
 			{#if new_password !== new_password2}
-				<p class="text-red-500 mt-2">As passwords não coincidem</p>
+				<p class="text-red-500 mt-2">A nova password não coincide.</p>
 				<div class="flex mt-5">
-					<button on:click|preventDefault><Button>Submit</Button></button>
+					<button on:click|preventDefault><Button>Gravar Alterações</Button></button>
+				</div>
+			{:else if new_password.length < 8 && new_password.length > 0}
+				<p class="text-red-500 mt-2">A password tem de ter pelo menos 8 caracteres.</p>
+				<div class="flex mt-5">
+					<button on:click|preventDefault><Button>Gravar Alterações</Button></button>
+				</div>
+			{:else if password.length <= 0}
+				<p class="text-red-500 mt-2">Insira a palavra-passe atual para gravar as alterações.</p>
+				<div class="flex mt-5">
+					<button on:click|preventDefault><Button>Gravar Alterações</Button></button>
+				</div>
+			{:else if error}
+				<p class="text-red-500 mt-2">Não foi possível atualizar os seus dados.</p>
+				<div class="flex mt-5">
+					<button on:click={updateData}><Button>Gravar Alterações</Button></button>
 				</div>
 			{:else}
 				<div class="flex mt-5">
-					<button on:click={showModal}><Button>Submit</Button></button>
+					<button on:click={updateData}><Button>Gravar Alterações</Button></button>
 				</div>
 			{/if}
 		</form>
-		{#if modal}
-			<Modal>Insira a sua password para confirmar a alteração dos dados</Modal>
-		{/if}
 	</div>
 </div>
