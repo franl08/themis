9212fc90524ce1c0cb6281d760ad90ed758c088e
franl08 <franeves08@gmail.com>
2023-06-18 12:10:48 +0100

    added error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,25 @@
 <script>
 	// @ts-nocheck
+	import '../app.css';
 
 	import { page } from '$app/stores';
+	import Button from '$lib/Button.svelte';
 </script>
 
-<h1>Something went wrong :(</h1>
-<h2>{$page.status}: {$page.error.message}</h2>
+<div class="flex flex-col mt-5">
+	<h1 class="flex text-4xl ml-20 text-pink-700 uppercase font-bold">
+		Parece-me que vamos ter de adiar o processo...
+	</h1>
+	<h2 class="flex text-3xl ml-20 text-pink-700 uppercase font-bold mt-6">
+		Erro {$page.status}: {$page.error.message}
+	</h2>
+	{#if $page.status === 404}
+		<div class="flex flex-col text-xl dark:text-white ml-36 mt-4">
+			<p>
+				Tentaste fugir ao caminho da justiça ou não tens permissões suficientes para ver esta
+				página. A Themis lamenta o incómodo.
+			</p>
+			<a class="mt-6" href="/"><Button>Voltar à Página Inicial</Button></a>
+		</div>
+	{/if}
+</div>
